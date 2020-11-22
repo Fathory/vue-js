8614f995f60925ec002d71d235abcaac84e67116
fathory <ggje33@gmail.com>
2020-11-22 18:24:47 +0900

    풀 페이지 스크롤 기능 추가

--- vue-client/src/components/HelloWorld.vue
+++ vue-client/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <a href="introduce/sublink"> subIntroduce </a>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
