dff5fe6b155083e0e6f6cd03e94898ef4c710201
fathory <ggje33@gmail.com>
2020-11-22 18:21:39 +0900

    기본 템플릿 추가, 필요시 참고용

---
+++ vue-client/src/components/layout/default.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+    export default {
+      name:"default_form",
+      component: [], // 연결할 컴포넌트를 추가해서 넣는다
+      data() { // 바인딩할 데이터를 선언한다.
+
+      },
+      methods: { // 사용할 메소드를 정의한다.
+
+      },
+      beforeCreate() { // DOM이 생성되기 전에 할 행위
+      },
+      created() { // DOM이 생성 완료된 시점에 할 행위
+      },
+      beforeMount() { // 데이터가 DOM에 마운트 되기 전에 할 행위
+
+      },
+      mounted() { // 데이터가 DOM에 마운트 된 시점에 할 행위
+      },
+      beforeUpdate() { // 데이터가 업데이트 되기 전에 할 행위
+      },
+      updated() { // 데이터가 업데이트 된 시점에 할 행위
+      },
+      beforeDestroy() { // 라우팅된 페이지에서 벗어나기 전에 할 행위 ! beforeUnmounted?
+      },
+      destroyed() { // 라우팅된 페이지에서 벗어난 시점에 할 행위 ! unmounted?
+      }
+    }
+</script>
